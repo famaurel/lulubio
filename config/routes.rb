20f56bb187b90583dfabb73a5cd4cdd7289e7c91
Fanny Maurel <fannymaurel@outlook.com>
2019-07-05 14:31:59 +0200

    Customize error pages

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
   get "produits", to: 'pages#produits'
   get "construction", to: 'pages#construction'
 
+  match "/404", to: "errors#not_found", via: :all
+  match "/422", to: "errors#unacceptable", via: :all
+  match "/500", to: "errors#internal_server_error", via: :all
+
   resources :contacts, only: [:new, :create]
 
   resources :ateliers, only: [:index, :new, :edit, :create, :update, :destroy]
